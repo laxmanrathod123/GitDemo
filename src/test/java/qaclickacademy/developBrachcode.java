9727fed73371d002683863695c024af34299bb44
Rathod <laxmanrathod2142@gmail.com>
2022-10-03 22:51:32 +0530

    practing the demo

---
+++ src/test/java/qaclickacademy/developBrachcode.java
@@ -0,0 +1,10 @@
+package qaclickacademy;
+
+public class developBrachcode {
+
+	public static void main(String[] args) {
+
+		System.out.println(" I am in develop branch and practicing the demos");
+	}
+
+}
