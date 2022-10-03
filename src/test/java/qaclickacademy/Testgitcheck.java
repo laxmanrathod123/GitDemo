42a5c69da0cb52b3d0dd6e91319318834bff7919
Rathod <laxmanrathod2142@gmail.com>
2022-10-03 16:08:40 +0530

    This is my second commit

---
+++ src/test/java/qaclickacademy/Testgitcheck.java
@@ -0,0 +1,13 @@
+package qaclickacademy;
+
+public class Testgitcheck {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Test git gemo");
+		System.out.println("Test git gemo");
+		System.out.println("Test git gemo");
+	}
+
+}
